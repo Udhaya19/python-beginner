3002d4ea6692fa19fd7e79acde898d8570bf4795
Admin <admin@Intern2-Macbook-Pro.local>
2019-01-29 14:26:43 +0530

    Day2 Program

--- Day2/function1.py
+++ Day2/function1.py
@@ -1,13 +1,17 @@
-
-num1 = int(input("Enter the number1"))
-num2 = int(input("Enter the number2"))
-
-def defal_param():
-    if num1 == num2:
-        print((num1 + num2) * 2)
+def defal_param(num1, num2=2):
+    user_value = str(input("Enter the yes/no :"))
+    if user_value == "yes":
+        num1 = int(input("Enter the number1 "))
+        num2 = int(input("Enter the number2 "))
+        if num1 == num2:
+            print((num1 + num2) * 2)
+        else:
+            print(num1 + num2)
     else:
-        print(num1 + num2)
+        if num1 == num2:
+            print((num1 + num2) * 2)
+        else:
+            print(num1 + num2)
 
 
-defal_param()
-
+print(defal_param(2, 3))
