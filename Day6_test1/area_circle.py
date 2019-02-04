c084355e13ef0ad878671b13e174886842214d3e
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-04 17:27:02 +0530

    Day6_test1

--- Day6_test1/area_circle.py
+++ Day6_test1/area_circle.py
@@ -1,9 +1,12 @@
 class Circle:
-    def find_area_perimeter(self,radius,pi):
+    def __init__(self,radius):
         self.radius=radius
-        self.pi=pi
-        print("area:",pi*radius**2)
-        print("Perimeter:",2*pi*radius)
 
-c=Circle()
-c.find_area_perimeter(3,3.14)
+    def find_area(self):
+        return 3.14*self.radius**2
+    def find_perimeter(self):
+        return 2*3.14*self.radius
+radius=int(input("Enter the radius:"))
+r=Circle(radius)
+print("Area of circle:",r.find_area())
+print("Perimeter of circle:",r.find_perimeter())
