57dbd4e4b25fa14c50a0b638f237554f084fd8f4
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-04 17:27:53 +0530

    Day6_test1

--- Day6_test1/count_item_list.py
+++ Day6_test1/count_item_list.py
@@ -1,2 +1,7 @@
-dictionary = { 1: [1, 2, 3], 2: [1, 2, 3, 4] ,3:"a"}
-print(len(dictionary.values()))
+dictionary = {1: [1, 2, 3], 2: [1, 2, 3, 4], 3: "a"}
+count = 0
+
+for values in dictionary.values():
+    if type(values) == list:
+        count += 1
+print(count)
