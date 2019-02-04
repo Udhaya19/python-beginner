ea64c622c4f5e4614e863e1b43dfb6258f9af17b
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-04 17:26:09 +0530

    Day6_test1

--- Day6_test1/remove_duplicate_element.py
+++ Day6_test1/remove_duplicate_element.py
@@ -1,9 +1,10 @@
 def remove_duplicate_item(elements):
-    list1=[]
+    list1 = []
     for i in elements:
         if i not in list1:
             list1.append(i)
     return list1
 
-elements=[1,2,3,2,1,25,6]
+
+elements = [1, 2, 3, 2, 1, 25, 6]
 print(remove_duplicate_item(elements))
