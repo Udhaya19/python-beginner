5d35a0c8a9d2f28c53c72be58889496872fe27c6
Admin <admin@Intern2-Macbook-Pro.local>
2019-02-06 14:05:25 +0530

    Day7_assignment

---
+++ Day7/composition_example.py
@@ -0,0 +1,21 @@
+class Department:
+    def __init__(self, dept_name, student_name):
+        self.dept_name = dept_name
+        self.student_name = student_name
+
+    def get_student_details(self):
+        print("Department :{} && Student name :{}".format(self.dept_name, self.student_name))
+
+
+class Institution:
+    def __init__(self, inst_name, Department):
+        self.inst_name = inst_name
+        self.pp = Department
+
+    def get_institution_details(self):
+        print("Institution details:", self.inst_name, self.pp.get_student_details())
+
+
+d = Department("CSE", "udhaya")
+i = Institution("Agni", d)
+i.get_institution_details()
